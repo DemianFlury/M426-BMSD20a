ff6c339ab94e286efac93aec4c22a7d3d4d9e891
Demian Flury <74318556+DemianFlury@users.noreply.github.com>
2022-10-26 15:25:12 +0200

    add mean tests and exception

--- Aufgaben/01-unittest/Statistics.Tests/AverageTest.cs
+++ Aufgaben/01-unittest/Statistics.Tests/AverageTest.cs
@@ -9,13 +9,32 @@ namespace Statistics.Tests
         [Fact]
         public void TestMeanOfTwoNumbers()
         {
-            List<int> numbers = new List<int> { 1, 3 };
-            double expected = 2;
+            List<int> numbers = new List<int> { 7, 8 };
+            double expected = 7.5;
             Average average = new Average();
 
             double actual = average.Mean(numbers);
 
             Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TestMeanOfFiveNumbers()
+        {
+            List<int> numbers = new List<int> { 7, 8, 9, 1, 3 };
+            double expected = 5.6;
+            Average average = new Average();
+
+            double actual = average.Mean(numbers);
+
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void TestMeanOfNoNumbers()
+        {
+            List<int> numbers = new List<int> { };
+            Average average = new Average();
+
+            Assert.Throws<ArgumentException>(() => average.Mean(numbers));
         }
     }
 }
